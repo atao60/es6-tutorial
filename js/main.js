aaa479b28d284a31ceed3cd302661a768d6e49f3
Pierre Raoul <p.m.raoul@gmail.com>
2020-04-14 02:34:08 +0200

    Using Arrow Functions

--- js/main.js
+++ js/main.js
@@ -3,10 +3,10 @@
 
 const defaultRate = 0;
 
-const calculateMonthlyPayment = function (principal, years, annualRate) {
-    if (! principal) { throw new Error("Initial capital must be provided")}
-    if (! years) { throw new Error("Term of the loan must be provided (years)")}
-    if (! annualRate || annualRate < 0) {
+const calculateMonthlyPayment = (principal, years, annualRate) => {
+    if (!principal) { throw new Error("Initial capital must be provided") }
+    if (!years) { throw new Error("Term of the loan must be provided (years)") }
+    if (!annualRate || annualRate < 0) {
         annualRate = defaultRate;
         console.log("No rate provided or negative one: zero rate will be taken in account")
     }
@@ -20,15 +20,37 @@ const calculateMonthlyPayment = function (principal, years, annualRate) {
         coeff = 1 / duration;
     }
     const monthlyPayment = principal * coeff;
-    
+
     return { principal, years, annualRate, monthlyPayment, monthlyRate };
 };
 
-document.getElementById('calcBtn').addEventListener('click', function () {
+const calculateAmortization = (principal, years, rate) => {
+    const { monthlyRate, monthlyPayment } = calculateMonthlyPayment(principal, years, rate);
+    const { amortization } = Array(+years).fill().reduce(({ balance, amortization }, _, y) => {
+        const term = Array(12).fill().reduce(({ interestY, principalY }, _, m) => {
+            const interestM = balance * monthlyRate;       //Interest payment for month m
+            const principalM = monthlyPayment - interestM; //Principal payment for month m
+            interestY += interestM;
+            principalY += principalM;
+            balance -= principalM;
+            if (balance < 0) {
+                principalY += balance;
+                balance = 0;
+            }
+            return { interestY, principalY };
+        }, { interestY: 0, principalY: 0 });
+        amortization.push(Object.assign({ balance }, term));
+        return { balance, amortization };
+    }, { balance: principal, amortization: [] });
+    return { monthlyPayment, monthlyRate, amortization };
+};
+
+document.getElementById('calcBtn').addEventListener('click', () => {
     const principal = document.getElementById('principal').value;
     const years = document.getElementById('years').value;
     const rate = document.getElementById('rate').value;
-    const { monthlyPayment, monthlyRate } = calculateMonthlyPayment(principal, years, rate);
+    const { monthlyPayment, monthlyRate, amortization } = calculateAmortization(principal, years, rate);
     document.getElementById('monthlyPayment').innerHTML = monthlyPayment.toFixed(2);
     document.getElementById('monthlyRate').innerHTML = (monthlyRate * 100).toFixed(2);
+    amortization.forEach(month => console.log(JSON.stringify(month)));
 });
